3454f908830b3645a981ae9f4cb87f662c918111
Steven Wright <sewright22@gmail.com>
2020-04-15 21:03:29 -0500

    Removed search button

--- com.stevencodeswright/DiabetesFoodJournal/DiabetesFoodJournal/Views/JournalPage.xaml.cs
+++ com.stevencodeswright/DiabetesFoodJournal/DiabetesFoodJournal/Views/JournalPage.xaml.cs
@@ -3,8 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamarinHelper.Core;
@@ -14,9 +14,35 @@ namespace DiabetesFoodJournal.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class JournalPage : ContentPage
     {
+        private System.Timers.Timer timer;
+
         public JournalPage()
         {
             InitializeComponent();
+            this.timer = new System.Timers.Timer(500);
+            this.timer.Elapsed += this.Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                this.timer.Stop();
+                this.searchButton.Command.Execute(this.searchEntry.Text.Trim());
+                this.loadingAnimation.IsRunning = false;
+                this.loadingAnimation.IsVisible = false;
+            });
+        }
+
+        private void searchEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.loadingAnimation.IsRunning = true;
+            this.loadingAnimation.IsVisible = true;
+            this.timer.Stop();
+            if (string.IsNullOrEmpty(this.searchEntry.Text.TrimEnd()) == false)
+            {
+                this.timer.Start();
+            }
         }
     }
 }
