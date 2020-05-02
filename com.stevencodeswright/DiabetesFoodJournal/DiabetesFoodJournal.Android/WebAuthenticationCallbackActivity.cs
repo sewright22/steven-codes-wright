52563a1df70ac03f59213f0d8be39432f33905a9
Steven Wright <sewright22@gmail.com>
2020-05-02 17:00:54 -0500

    Officially using oAuth2

--- com.stevencodeswright/DiabetesFoodJournal/DiabetesFoodJournal.Android/WebAuthenticationCallbackActivity.cs
+++ com.stevencodeswright/DiabetesFoodJournal/DiabetesFoodJournal.Android/WebAuthenticationCallbackActivity.cs
@@ -10,15 +10,19 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using DiabetesFoodJournal.Services;
+using Xamarin.Forms;
 
 namespace DiabetesFoodJournal.Droid
 {
 
+
     [Activity(NoHistory = true, LaunchMode = LaunchMode.SingleTop)]
     [IntentFilter(new[] { Intent.ActionView },
-    Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
-    DataScheme = "DiabetesFoodJournal")]
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+        DataScheme = CALLBACK_SCHEME)]
     public class WebAuthenticationCallbackActivity : Xamarin.Essentials.WebAuthenticatorCallbackActivity
     {
+        const string CALLBACK_SCHEME = "com.stevencodeswright.diabetesfoodjournal";
     }
 }
--- com.stevencodeswright/DiabetesFoodJournal/DiabetesFoodJournal/Services/DexcomDataStore.cs
+++ com.stevencodeswright/DiabetesFoodJournal/DiabetesFoodJournal/Services/DexcomDataStore.cs
@@ -5,26 +5,37 @@
     using System.Net.Http;
     using System.Net.Http.Headers;
     using System.Text;
-    using RestSharp;
     using Xamarin.Auth;
     using System.Diagnostics;
     using Newtonsoft.Json;
     using DiabetesFoodJournal.Models;
+    using System.Collections.Generic;
 
     public class DexcomDataStore : IDexcomDataStore
     {
+        private string token;
+        private string refreshToken;
+        private string baseUrl = "https://sandbox-api.dexcom.com";
+        private string client_id = "WWv2aPRKLsm9SAEgTcrIg8anRiHKbv5e";
+        private string client_secret = "x4LTtwqRWJMiaubT";
+        private string redirectString = "com.stevencodeswright.diabetesfoodjournal://";
+        private string callback = "com.stevencodeswright.diabetesfoodjournal://";
+        private string scope = "offline_access";
 
-        string _baseUrl = "https://sandbox-api.dexcom.com";
-        string _token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI5MzdjMjBjNy0yOThlLTQzNzgtODhiYS0zOWVhOGQ1MzIwMDgiLCJhdWQiOiJodHRwczovL3NhbmRib3gtYXBpLmRleGNvbS5jb20iLCJzY29wZSI6WyJlZ3YiLCJjYWxpYnJhdGlvbiIsImRldmljZSIsImV2ZW50Iiwic3RhdGlzdGljcyIsIm9mZmxpbmVfYWNjZXNzIl0sImlzcyI6Imh0dHBzOi8vc2FuZGJveC1hcGkuZGV4Y29tLmNvbSIsImV4cCI6MTU4ODEyNjczMiwiaWF0IjoxNTg4MTE5NTMyLCJjbGllbnRfaWQiOiJXV3YyYVBSS0xzbTlTQUVnVGNySWc4YW5SaUhLYnY1ZSJ9.bPvtDfGuYzynBo7alJdUeYUMD4_4zkg5tSR_dsWfIAzppinh096swVI8nD4jWO_pi5KRgmh8O28bmEygH93rv8cfQJaUNjlXMkcCf0b2G8MyrDhyegpGFCDgKOvRYeK9U39hoEI_GYnIX58liRve5d4USPObqBw1pKSEIlMIGo-cKQ2p4Sv2Cu5MaJMsPuFy8lLZFuVq4cElAkWKRqZ8S0EIQyM5N8CsNsgQDQ9245ADw8vRRSLeDOAeu3ymBbDqdxQqFp1qKVOKp0A9DQXepE1Dz49MMr7HLG99UQAnuMC5YIJ-qQSsTkwYVHuIAI9H1Avr9AJ5BoJdRjpwkvcPTg";
         public async Task<ReadingList> GetEGV(DateTime startTime, DateTime endTime)
         {
+            if (string.IsNullOrEmpty(token) && string.IsNullOrEmpty(refreshToken))
+            {
+                await this.Login();
+            }
+
             // Login();
             var client = new HttpClient();
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            client.DefaultRequestHeaders.Add("authorization", "Bearer " + _token);
+            client.DefaultRequestHeaders.Add("authorization", "Bearer " + token);
             //var url = _baseUrl + "/v2/users/self/egvs?startDate=2020-02-22T16:08:19.514&endDate=2020-02-27T12:03:33.967";
-            var url = _baseUrl + $"/v2/users/self/egvs?startDate={startTime.ToString("yyyy-MM-ddTHH:mm:ss.sss")}&endDate={endTime.ToString("yyyy-MM-ddTHH:mm:ss.sss")}";
+            var url = baseUrl + $"/v2/users/self/egvs?startDate={startTime.ToString("yyyy-MM-ddTHH:mm:ss")}&endDate={endTime.ToString("yyyy-MM-ddTHH:mm:ss")}";
             using (HttpResponseMessage response = await client.GetAsync(url))
             {
                 var result = await response.Content.ReadAsStringAsync();
@@ -33,32 +44,44 @@
             }
         }
 
-        private void Login()
+        public async Task Login()
         {
-            var oauth = new OAuth2Authenticator("WWv2aPRKLsm9SAEgTcrIg8anRiHKbv5e", "x4LTtwqRWJMiaubT", "offline_access", new Uri("https://sandbox-api.dexcom.com/v2/oauth2/login"), new Uri("DiabetesFoodJournal://"), new Uri("https://sandbox-api.dexcom.com/v2/oauth2/token"));
-            oauth.Completed += Oauth_Completed;
-            oauth.Error += Oauth_Error;
-           //AuthenticationState.Authenticator = authenticator;
+            var oauth = await Xamarin.Essentials.WebAuthenticator.AuthenticateAsync(
+                                                                   new Uri($"https://sandbox-api.dexcom.com/v2/oauth2/login?client_id={client_id}&redirect_uri={redirectString}&response_type=code&scope={scope}"),
+                                                                   new Uri(callback));
 
-            var presenter = new Xamarin.Auth.Presenters.OAuthLoginPresenter();
-            presenter.Login(oauth);
-        }
+            var accessToken = oauth?.Get("code");
 
-        private void Oauth_Error(object sender, AuthenticatorErrorEventArgs e)
-        {
-            var test = 5;
-            var test2 = test;
-        }
+            var client = new HttpClient();
+            var parameters = new Dictionary<string, string>
+            {
+                { "client_secret", client_secret }
+              , { "client_id", client_id }
+              , { "code", accessToken}
+              , { "grant_type", "authorization_code"}
+              , { "redirect_uri", redirectString}
+            };
 
-        private void Oauth_Completed(object sender, AuthenticatorCompletedEventArgs e)
-        {
-            var test = 5;
-            var test3 = test;
+            var encodedContent = new FormUrlEncodedContent(parameters);
+
+            //encodedContent.Headers.Add("content-type", "application/x-www-form-urlencoded");
+            client.DefaultRequestHeaders.Add("cache-control", "no-cache");
+            //request.AddParameter("application/x-www-form-urlencoded", "client_secret={your_client_secret}&client_id={your_client_id}&code={your_authorization_code}&grant_type=authorization_code&redirect_uri={your_redirect_uri}", ParameterType.RequestBody);
+            using (var response = await client.PostAsync("https://sandbox-api.dexcom.com/v2/oauth2/token", encodedContent))
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var item = JsonConvert.DeserializeAnonymousType(result, new
+                { access_token = "", expires_in = 0, token_type = "", refresh_token = "" });
+
+                token = item.access_token;
+                refreshToken = item.refresh_token;
+            }
         }
     }
 
     public interface IDexcomDataStore
     {
         Task<ReadingList> GetEGV(DateTime startTime, DateTime endTime);
+        Task Login();
     }
 }
