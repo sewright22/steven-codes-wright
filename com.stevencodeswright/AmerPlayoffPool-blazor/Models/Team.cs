8f9040ae716c46c15fce6100094bd926038bf839
Steven Wright <sewright22@gmail.com>
2020-07-13 18:51:08 -0500

    More testing.

--- com.stevencodeswright/AmerPlayoffPool-blazor/Models/Team.cs
+++ com.stevencodeswright/AmerPlayoffPool-blazor/Models/Team.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,7 @@ namespace AmerPlayoffPool_blazor.Models
     public class Team
     {
         public string Name;
+        public string PrimaryColor;
+        public Color SecondaryColor;
     }
 }
