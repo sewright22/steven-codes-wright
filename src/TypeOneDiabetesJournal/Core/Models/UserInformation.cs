ad5f1664a82d6d02eb8303488d48277be3eb0d9f
sewright22 <sewright22@gmail.com>
2023-07-20 21:27:44 -0500

    Updated to only build the csproj.

--- src/TypeOneDiabetesJournal/Core/Models/UserInformation.cs
+++ src/TypeOneDiabetesJournal/Core/Models/UserInformation.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Core.Models;
 
-namespace Core.Models
+public class UserInformation
 {
-    public class UserInformation
-    {
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string? Email { get; set; }
-        public string? UserName { get; set; }
-        public string? Token { get; set; }
-    }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? UserName { get; set; }
+    public string? Token { get; set; }
 }
