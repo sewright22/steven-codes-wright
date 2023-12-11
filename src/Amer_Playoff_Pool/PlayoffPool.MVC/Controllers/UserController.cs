79bc1b6b8feaa1ca10a9167bcf760e62eff4937c
Steven Wright <sewright22@gmail.com>
2023-12-10 20:13:19 -0600

    Created UserController

--- src/Amer_Playoff_Pool/PlayoffPool.MVC/Controllers/UserController.cs
+++ src/Amer_Playoff_Pool/PlayoffPool.MVC/Controllers/UserController.cs
@@ -1,9 +1,17 @@
 ﻿namespace PlayoffPool.MVC.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using PlayoffPool.MVC.Helpers;
 
     public class UserController : Controller
     {
+        public UserController(ILogger<UserController> logger, IDataManager dataManager)
+        {
+            this.Logger = logger;
+        }
+
+        public ILogger<UserController> Logger { get; }
+
         public IActionResult Index()
         {
             return View();
