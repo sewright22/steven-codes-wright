81fbec8cdf7a4933d18e950b1ff7089708a038ae
Steven Wright <sewright22@gmail.com>
2023-12-10 20:07:11 -0600

    Updated manage users section to use a partial view.

---
+++ src/Amer_Playoff_Pool/PlayoffPool.MVC/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿namespace PlayoffPool.MVC.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
