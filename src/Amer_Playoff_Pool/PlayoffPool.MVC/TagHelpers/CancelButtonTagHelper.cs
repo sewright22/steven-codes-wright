0126576866ebfbc62a1a026039b7534119fcc72a
Steven Wright <sewright22@gmail.com>
2023-12-24 10:10:09 -0600

    Added cancel button tag helper.

---
+++ src/Amer_Playoff_Pool/PlayoffPool.MVC/TagHelpers/CancelButtonTagHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace PlayoffPool.MVC.TagHelpers
+{
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+    using PlayoffPool.MVC.Extensions;
+
+    public class CancelButtonTagHelper : TagHelper
+    {
+        public string? TagName { get; }
+        public string? Text { get; set; }
+        public string? CssClass { get; set; }
+
+        public CancelButtonTagHelper()
+        {
+            this.TagName = "button";
+            this.Text = "Cancel";
+            this.CssClass = "btn pp-button pp-button-secondary";
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = this.TagName;
+
+            if (this.CssClass.HasValue())
+            {
+                output.Attributes.Add("class", this.CssClass);
+            }
+
+            output.Attributes.Add("data-bs-dismiss", "modal");
+
+            output.Content.SetContent(this.Text);
+        }
+    }
+}
