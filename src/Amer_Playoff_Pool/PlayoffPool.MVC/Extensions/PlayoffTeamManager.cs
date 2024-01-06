3c237660f11012f1ec960dc5b156d6ccd51de194
Steven Wright <sewright22@gmail.com>
2024-01-06 13:41:57 -0600

    Fixed bug causing duplicate team to be added.

--- src/Amer_Playoff_Pool/PlayoffPool.MVC/Extensions/PlayoffTeamManager.cs
+++ src/Amer_Playoff_Pool/PlayoffPool.MVC/Extensions/PlayoffTeamManager.cs
@@ -31,28 +31,6 @@
                 });
         }
 
-        //public static IQueryable<PlayoffTeamModel> GetPlayoffTeams(this AmerFamilyPlayoffContext dbContext, int seasonId)
-        //{
-        //    return dbContext.PlayoffTeams.AsNoTracking()
-        //        .Where(x => x.Playoff.SeasonId == seasonId)
-        //    .Select(
-        //        x => new PlayoffTeamModel
-        //        {
-        //            Id = x.Id,
-        //            Name = x.SeasonTeam.Team.Name,
-        //            Abbreviation = x.SeasonTeam.Team.Abbreviation,
-        //            Seed = x.Seed,
-        //            ConferenceId = x.SeasonTeam.ConferenceId,
-        //            Conferences = dbContext.Conferences.AsNoTracking()
-        //            .Select(
-        //                y => new SelectListItem
-        //                {
-        //                    Value = y.Id.ToString(),
-        //                    Text = y.Name,
-        //                }).ToList(),
-        //        });
-        //}
-
         public static void AddPlayoffTeam(this AmerFamilyPlayoffContext dataContext, PlayoffTeamModel model)
         {
             if (dataContext is null)
@@ -69,7 +47,6 @@
             {
                 throw new ArgumentException("Team is required.", nameof(model));
             }
-
 
             // Find Team
             var team = dataContext.Teams.FirstOrDefault(x => x.Id == model.TeamId);
@@ -90,10 +67,10 @@
                     SeasonId = model.SeasonId,
                     ConferenceId = model.ConferenceId,
                 };
-            }
 
-            dataContext.SeasonTeams.Add(seasonTeam);
-            dataContext.SaveChanges();
+                dataContext.SeasonTeams.Add(seasonTeam);
+                dataContext.SaveChanges();
+            }
 
             // Find PlayoffTeam
             var playoffTeam = dataContext.PlayoffTeams.FirstOrDefault(x => x.SeasonTeamId == seasonTeam.Id);
